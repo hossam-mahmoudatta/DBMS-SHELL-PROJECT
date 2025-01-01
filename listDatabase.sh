84d80e302caf0165f5ba86b9c28415f3140f0b3e
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2025-01-01 17:25:00 +0200

    Coded the listing of databases logic

--- listDatabase.sh
+++ listDatabase.sh
@@ -2,30 +2,20 @@
 # Created by Hossam
 
 listDatabase() {
-    # Prompt the user for a database name using Zenity
-    db_name=$(zenity --entry --title="Create Database" --text="Enter your database name:")
+    dir="DATABASES/"  # Replace with your directory path
 
-    # Check if the user canceled the operation
-    if [ $? -ne 0 ]; then
-        zenity --error --title="Operation Canceled" --text="No database name entered."
-        return
-    fi
+    # Get the list of files in the directory
+    databasesLists=$(ls "$dir")
 
-    # Validate the database name (no spaces, only alphanumeric characters, underscores, or camelCase)
-    if [[ ! "$db_name" =~ ^[a-zA-Z0-9_]+$ ]]; then
-        zenity --error --title="Invalid Database Name" --text="Invalid database name. Avoid spaces and special characters. Use underscores (_) or camelCase if necessary."
-        return
-    fi
-
-    # Check if the directory already exists
-    if [ -d "$db_name" ]; then
-        zenity --warning --title="Database Exists" --text="Database \"$db_name\" already exists!"
+    # Check if the directory contains files
+    if [ -z "$databasesLists" ];
+    then
+        zenity --info --text="You don't have any databases!"
     else
-        # Create the directory for the database
-        mkdir -p DATABASES/"$db_name"
-        zenity --info --title="Database Created" --text="Database \"$db_name\" created successfully."
+        # Use Zenity to display the contents in a list
+        zenity --list --title="Your Databases" --text="Here are your databases" --column="Items" $databasesLists
     fi
 }
 
 # Call the function
-createDatabase
+listDatabase

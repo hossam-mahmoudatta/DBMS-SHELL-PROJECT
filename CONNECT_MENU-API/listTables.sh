afafb45d6af66786d9c282cf67bee28e65d7bef7
ZiadTarek10 <ziadtarekmohamed32@gmail.com>
2025-01-02 19:31:05 +0200

    secondCommit

---
+++ CONNECT_MENU-API/listTables.sh
@@ -0,0 +1,21 @@
+# Created: 2025-01-01 15:00:00
+# Created by Hossam
+
+listDatabase() {
+    dir="../DATABASES/"
+
+    # Get the list of files in the directory
+    databasesLists=$(ls "$dir")
+
+    # Check if the directory contains files
+    if [ -z "$databasesLists" ];
+    then
+        zenity --info --text="You don't have any databases!"
+    else
+        # Use Zenity to display the contents in a list
+        zenity --list --title="Your Databases" --text="Here are your databases" --column="Items" $databasesLists
+    fi
+}
+
+# Call the function
+listTables "$1"

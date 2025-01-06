c9e417658ab0a70aec185c075de21e2777586e94
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2025-01-06 14:39:35 +0200

    adding a new API to make code more manageable

--- CONNECT_MENU-API/insertTable.sh
+++ CONNECT_MENU-API/insertTable.sh
@@ -16,23 +16,18 @@ insertTable() {
     then
         zenity --info --text="You don't have any Tables!"
     else
-    # Display the databases in a clickable list
+        # Display the databases in a clickable list
         selectedTable=$(zenity --list --title="Your Tables" --text="Select a Table:" --column="Tables" $tablesList)
 
-        # Check if a database was selected
+        # Check if a table was selected
         if [ -n "$selectedTable" ];
         then
             # Navigate to the next menu based on the selected database
-            zenity --info --text="You selected the database: $selectedDB"
-            
-            # Call a function or script for the next menu
-            # echo "Current Directory: $(pwd)"
-            # echo "Current Directory: $selectedDB"
-            # echo "Current Directory: $dir/$selectedDB"
-
-            ../CONNECT_MENU-API/connectMenu.sh "$dir/$selectedDB"
-        # Use Zenity to display the contents in a list
-        zenity --list --title="Your Tables" --text="Here are your Tables" --column="Items" $tablesList
+            zenity --info --text="You selected the Table: $selectedTable"
+        else
+            zenity --warning --text="No database selected."
+        fi
+           
     fi
     # Define the database path
     dbPath=$1
@@ -78,47 +73,4 @@ insertTable() {
 
 # Call the function 
 insertTable $1
-
-
-
-connectDatabase() {
-    # Replace with your directory path
-    dir="../DATABASES"
-    # currentDIR=$(dirname "$0")
-
-    # Get the list of files in the directory
-    databasesLists=$(ls "$dir")
-
-    # Check if the directory contains files
-    if [ -z "$databasesLists" ];
-    then
-        zenity --info --text="You don't have any databases!"
-    else
-        # Display the databases in a clickable list
-        selectedDB=$(zenity --list --title="Your Databases" --text="Select a Database:" --column="Databases" $databasesLists)
-
-        # Check if a database was selected
-        if [ -n "$selectedDB" ];
-        then
-            # Navigate to the next menu based on the selected database
-            zenity --info --text="You selected the database: $selectedDB"
-            
-            # Call a function or script for the next menu
-            # echo "Current Directory: $(pwd)"
-            # echo "Current Directory: $selectedDB"
-            # echo "Current Directory: $dir/$selectedDB"
-
-            ../CONNECT_MENU-API/connectMenu.sh "$dir/$selectedDB"
-        else
-            zenity --warning --text="No database selected."
-        fi
-    fi
-}
-
-
-
-
-
-
-
   
---
+++ CONNECT_MENU-API/pickTable.sh
@@ -0,0 +1,36 @@
+# Pick from a Table API 
+# Hossam Mahmoud
+
+#########################################################
+#########################################################
+
+# This API should allow us to view our tables and
+# pick from it to allow further processing
+pickTable() {
+    dbPath=$1
+    dir="$dbPath/TABLES/"
+    # Get the list of files in the directory
+    tablesList=$(ls $dir)
+
+    # Check if the directory contains files
+    if [ -z "$tablesList" ];
+    then
+        zenity --info --text="You don't have any Tables!"
+    else
+        # Display the databases in a clickable list
+        selectedTable=$(zenity --list --title="Your Tables" --text="Select a Table:" --column="Tables" $tablesList)
+
+        # Check if a table was selected
+        if [ -n "$selectedTable" ];
+        then
+            # Navigate to the next menu based on the selected database
+            zenity --info --text="You selected the Table: $selectedTable"
+        else
+            zenity --warning --text="No database selected."
+        fi
+    fi
+}
+
+# Call the function 
+pickTable $1
+

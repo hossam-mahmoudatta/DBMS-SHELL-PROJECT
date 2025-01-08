7f5ac33d049638b87dfd9f9e3f5cddde22116cdc
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2025-01-08 14:55:23 +0200

    Finished creating log for the mainMenu

--- MAIN_MENU-API/mainMenu.sh
+++ MAIN_MENU-API/mainMenu.sh
@@ -8,6 +8,10 @@
 # Hossam Mahmoud
 
 mainMenu() {
+    logFile="../LOGS/mainMenu.log"
+
+    echo "$(date) - Main Menu Started" >> $logFile
+
     choice=$(zenity --list \
         --title="ITI HZA DBMS Main Menu" \
         --text="Choose your preferred action:" \
@@ -18,35 +22,45 @@ mainMenu() {
         "Connect to a Database" \
         "Drop a Database" \
         "Exit" --extra-button="Exit")
-    
+
     case $choice in
         "Create a Database")
+            echo "$(date) - Create Database Option Selected" >> $logFile
             ./createDatabase.sh
+            echo "$(date) - Create Database Option Executed" >> $logFile
             mainMenu
             ;;
         "List Databases")
+            echo "$(date) - List Databases Option Selected" >> $logFile
             ./listDatabase.sh
+            echo "$(date) - List Databases Option Executed" >> $logFile
             mainMenu
             ;;
         "Connect to a Database")
+            echo "$(date) - Connect to Database Option Selected" >> $logFile
             ./connectDatabase.sh
+            echo "$(date) - Connect to Database Option Executed" >> $logFile
             mainMenu
             ;;
         "Drop a Database")
+            echo "$(date) - Drop Database Option Selected" >> $logFile
             ./dropDatabase.sh
+            echo "$(date) - Drop Database Option Executed" >> $logFile
             mainMenu
             ;;
         "Exit")
+            echo "$(date) - Exit Option Selected" >> $logFile
             zenity --info --text="Thank you for using HZA DBMS!" --title="Exit"
+            echo "$(date) - Application Exited" >> $logFile
             exit 0
             ;;
         *)
+            echo "$(date) - Invalid Option Selected" >> $logFile
             zenity --error --text="Invalid option. Please Try again." --title="Error"
             mainMenu
             ;;
     esac
 }
-
 
 # Start the application
 mainMenu
